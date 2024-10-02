a54ec1035ac532e57717bc561a90211ff921fa11
Felix Wotschofsky <felix@wotschofsky.com>
2024-10-02 13:14:13 +0200

    Rework import script to use mongo dump 📦

--- import.ts
+++ import.ts
@@ -1,53 +1,71 @@
 import { getPublicSuffix } from 'tldts';
+import readline from 'node:readline';
+import fs from 'node:fs';
 
-import { mongo } from './lib/mongo';
 import { sql } from './lib/postgres';
 
 const CHUNK_SIZE = 32_500;
 
-const db = mongo.db('certstream');
-const baseDomains = db.collection('baseDomains');
+// Format a number with thousands separators
+const formatNumber = (input: number) =>
+  input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+if (!process.env.INPUT_PATH) {
+  console.error('INPUT_PATH environment variable is required');
+  process.exit(1);
+}
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(process.env.INPUT_PATH),
+  crlfDelay: Infinity,
+});
 
 const main = async () => {
-  console.log('Estimating total entries count...');
-  const totalEntries = await baseDomains.estimatedDocumentCount();
-  console.log(`Found ${totalEntries} entries`);
+  let totalInserted = 0;
+  let rawChunk: string[] = [];
 
-  let lastValue = null;
-  for (let i = 0; true; i++) {
-    const query = lastValue ? { value: { $gt: lastValue } } : {};
+  for await (const line of rl) {
+    rawChunk.push(line);
 
-    const chunk = await baseDomains
-      .find(query, { projection: { value: 1 } })
-      .sort({ value: 1 })
-      .limit(CHUNK_SIZE)
-      .toArray();
-
-    if (chunk.length === 0) {
-      break;
+    if (rawChunk.length === CHUNK_SIZE) {
+      await processChunk(rawChunk);
+      totalInserted += rawChunk.length;
+      console.log(`Inserted ${formatNumber(totalInserted)} domains`);
+      rawChunk = [];
     }
+  }
 
-    const data = chunk
-      .map((entry) => ({
-        domain: entry.value,
-        tld: getPublicSuffix(entry.value),
-      }))
-      .filter((entry): entry is { domain: string; tld: string } =>
-        Boolean(entry.tld)
-      );
-
-    await sql`INSERT INTO domains ${sql(data)}`;
-    console.log(
-      `Inserted ${(i + 1) * CHUNK_SIZE}/~${totalEntries} domains (${
-        (~((i + 1) * CHUNK_SIZE) / totalEntries) * 100
-      }%)`
-    );
-
-    lastValue = chunk[chunk.length - 1].value;
+  // Process remaining lines
+  if (rawChunk.length > 0) {
+    await processChunk(rawChunk);
+    totalInserted += rawChunk.length;
+    console.log(`Inserted ${formatNumber(totalInserted)} domains`);
   }
 };
 
-mongo
-  .connect()
-  .then(main)
-  .then(() => process.exit(0));
+const processChunk = async (rawChunk: string[]) => {
+  const domains = rawChunk
+    .map((line) => {
+      try {
+        return JSON.parse(line) as { value: string };
+      } catch (e) {
+        console.error(`Failed to parse line: ${line}`);
+        return null;
+      }
+    })
+    .map((e) => e?.value)
+    .filter((e): e is string => Boolean(e));
+
+  const data = domains
+    .map((domain) => ({
+      domain: domain,
+      tld: getPublicSuffix(domain),
+    }))
+    .filter((entry): entry is { domain: string; tld: string } =>
+      Boolean(entry.tld)
+    );
+
+  await sql`INSERT INTO domains ${sql(data)}`;
+};
+
+main().then(() => process.exit(0));
--- lib/mongo.ts
+++
@@ -1,5 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-export const mongo = new MongoClient(process.env.MONGO_URL!);
-
-mongo.connect();
