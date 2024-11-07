fc752075e21978e8f1eeb9dc5472ada6457dc5a9
Felix Wotschofsky <felix@wotschofsky.com>
2024-11-07 21:36:50 +0100

    Fix unlocking on error changing analyzed_at value ✅

--- app.ts
+++ app.ts
@@ -103,7 +103,7 @@ const processEntry = async () => {
     if (!result) {
       await sql`
         UPDATE domains
-        SET analyzed_at = NOW()
+        SET domain = ${domain}
         WHERE domain = ${domain};
       `;
       return false;
