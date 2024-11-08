8fca25c1fffbeb3ace8c630188e424ce010230eb
Felix Wotschofsky <felix@wotschofsky.com>
2024-11-08 18:20:26 +0100

    reanalyze where registrar null

--- app.ts
+++ app.ts
@@ -87,7 +87,7 @@ const processEntry = async () => {
     const rows = await sql`
       SELECT domain
       FROM domains
-      WHERE registrar = 'unknown'
+      WHERE (registrar IS NULL OR registrar = 'unknown')
       AND NOT (tld = ANY(${sql.array(UNSUPPORTED_TLDS, 1043)}))
       LIMIT 1
       FOR UPDATE SKIP LOCKED;
